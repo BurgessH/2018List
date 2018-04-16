93cda13538eda1b62ff7d1b7e48fb981ac911cfa
HeMeng <2042877026@qq.com>
2018-04-16 21:24:26 +0800

    Update Ubuntu_Applay.c

--- Ubuntu_Applay.c
+++ Ubuntu_Applay.c
@@ -61,5 +61,42 @@
   ## 8.adcanced:
     - choose point-to-point encryption(MMPE)
    
+ # Centos6 server:
+  Step 1. First install pptpd
+    yum install ppp iptables nano
+    #For 64bit OS
+        wget http://poptop.sourceforge.net/yum/stable/packages/pptpd-1.4.0-1.el6.x86_64.rpm
+        rpm -Uhvpptpd-1.4.0-1.el6.x86_64.rpm
+    #For 32bit os
+        wget http://poptop.sourceforge.net/yum/stable/packages/pptpd-1.4.0-1.el6.i686.rpm
+        rmp -Uhv pptpd-1.4.0-1.el6.i686.rpm
         
+   Step 2.setup pptpd
+        edit ip settings in /etc/pptpd.conf
+        #nano /etc/pptpd.conf
+        localip 192.168.0.1 # your VPS/Dedicated Server IP address 
+        remoteip 192.168.0.101-200
+  And the following settings to /etc/ppp/options.pptpd:
+        ms-dns 8.8.8.8
+        ms-dns 4.4.4.4
+            
+   Step 3. Create user to access the VPN server
+    Add user account in/etc/ppp/chap-secrets (assign username and password):
+        #nano /etc/ppp/chap-secrets
+        vpn pptpd vpnpassword *
+        
+   Step 4. Enable network forwarding in /etc/sysctl.conf
+        #nano /etc/sysctl.conf
+        net.ipv4.ip_forward = 1
+        #To make the changes to sysctl.conf take effect, use the following command.
+        sysctl -p
+        
+   Step 5.Setup iptables
+        iptables -A INPUT -i eth0 -p tcp --dport 1723 -j ACCEPT
+        iptables -A INPUT -i eth0 -p gre -j ACCEPT
+        iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+        
+   Step 6.Start PPTP VPN server
+        service pptpd restart
+        chkconfig pptpd on
   
